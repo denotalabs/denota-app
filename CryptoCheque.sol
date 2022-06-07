a7a86292377a5e12457d508307246e912306def5
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2022-06-07 10:15:58 -0400

    add releasing of funds after voiding by reviewer to an account trusted by the user

--- CryptoCheque.sol
+++ CryptoCheque.sol
@@ -15,20 +15,30 @@ contract CryptoCheque {
     mapping(address=>mapping(address=>bool)) public acceptedReviewer;  // Merchent=>Reviewer
     mapping(address=>mapping(address=>bool)) public signerReviewer;  // Signer=>Reviewer
     mapping(address=>mapping(uint256=>bool)) public reviewerDurations;  // Reviewer
+    mapping(address=>address) public trustedAccount;  // User's trusted account
+    mapping(address=>uint256) public lastTrustedChange;
     uint256 totalSupply;
 
     function deposit() payable external {
         balances[msg.sender] += msg.value;
     }
-    function writeCheck(uint256 amount, uint256 duration, address reviewer) external{
+    function writeCheque(uint256 amount, uint256 duration, address reviewer) public{
         require(reviewerDurations[reviewer][duration], "Reviewer doesn't allow this duration");
         require(signerReviewer[msg.sender][reviewer], "Reviewer must approve this account");
         balances[msg.sender] -= amount;
         totalSupply += 1;
         cheques[totalSupply+1] = Cheque({drawer: msg.sender, bearer: msg.sender, expiry: block.timestamp+duration, 
+                                         reviewer: reviewer, amount: amount, voided: false, transferable: true});
+    }
+    function writeCheque(uint256 amount, uint256 duration, address reviewer, address to) external{
+        require(reviewerDurations[reviewer][duration], "Reviewer doesn't allow this duration");
+        require(signerReviewer[msg.sender][reviewer], "Reviewer must approve this account");
+        balances[msg.sender] -= amount;
+        totalSupply += 1;
+        cheques[totalSupply+1] = Cheque({drawer: msg.sender, bearer: to, expiry: block.timestamp+duration, 
                                        reviewer: reviewer, amount:amount, voided:false, transferable:true});
     }
-    function cashCheck(uint256 chequeID) external{  // Only allow withdraws via cheque writing
+    function cashCheque(uint256 chequeID) external{  // Only allow withdraws via cheque writing
         Cheque storage cheque = cheques[chequeID];
         require(cheque.bearer==msg.sender, "Must own cheque to cash");
         require(cheque.expiry>block.timestamp, "Cheque not cashable yet");
@@ -36,12 +46,19 @@ contract CryptoCheque {
         cheque.voided = true;
         this.transfer(cheque.bearer, cheque.amount);
     }
-    function voidCheck(uint256 chequeID) external{
+    function voidCheque(uint256 chequeID) external{
         Cheque storage cheque = cheques[chequeID];
         require(cheque.reviewer==msg.sender, "Must be reviewer");
         require(cheque.expiry<block.timestamp, "Cheque already matured");
         cheque.voided = true;
         balances[cheque.drawer] += cheque.amount;  // Add balance back to signer
+    }
+    function voidTransferCheque(uint256 chequeID) external {
+        Cheque storage cheque = cheques[chequeID];
+        require(cheque.reviewer==msg.sender, "Must be reviewer");
+        require(cheque.expiry<block.timestamp, "Cheque already matured");
+        cheque.voided = true;
+        balances[trustedAccount[cheque.drawer]] += cheque.amount;  // Add balance back to trusted (secondary) account
     }
     function transfer(address to, uint256 chequeID) external{
         Cheque storage cheque = cheques[chequeID];
@@ -61,7 +78,11 @@ contract CryptoCheque {
     function setAcceptedSigners(address signer) external{  // Reviewer will set this
         signerReviewer[msg.sender][signer] = true;
     }
-
+    function setTrustedAccount(address account) external {
+        require((lastTrustedChange[msg.sender] + 30 days)<block.timestamp, "Can only change trusted account once every 30 days");
+        trustedAccount[msg.sender] = account;
+        lastTrustedChange[msg.sender] += block.timestamp;
+    }
     function getChequeDrawer(uint256 chequeId) external view returns (address){
         return cheques[chequeId].drawer;
     }
