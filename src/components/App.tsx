8ddf3c194d6c3aaf7ea18be0a6a6be3b7c01a572
Rafi <rafi337161@gmail.com>
2022-08-27 20:22:17 -0400

    formatted app.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -24,30 +24,29 @@ function App() {
       <Navbar blockchainState={blockchainState} />
 
       <div className="container-fluid mt-5 text-center">
-        
         <br></br>
         <h1>Welcome to Cheq</h1>
         <h6>Total cheqs written: {blockchainState.cheqBalance}</h6>
         <h6>Total weth deposited: {blockchainState.wethBalance}</h6>
         <h6>Total dai deposited: {blockchainState.daiBalance}</h6>
         <br></br>
-        
-            <div className="tabs">
-              <Tabs id="uncontrolled-tab-example">
-                <Tab eventKey="deposit" title="Deposit">
-                  <DepositTab blockchainState={blockchainState} />
-                </Tab>
-                <Tab eventKey="write" title="Write">
-                  <WriteTab blockchainState={blockchainState} />
-                </Tab>
-                <Tab eventKey="cash" title="Cash">
-                  <CashTab blockchainState={blockchainState} />
-                </Tab>
-                <Tab eventKey="Auditors" title="Auditors">
-                  <AuditorsTab blockchainState={blockchainState} />
-                </Tab>
-              </Tabs>
-            </div>
+
+        <div className="tabs">
+          <Tabs id="uncontrolled-tab-example">
+            <Tab eventKey="deposit" title="Deposit">
+              <DepositTab blockchainState={blockchainState} />
+            </Tab>
+            <Tab eventKey="write" title="Write">
+              <WriteTab blockchainState={blockchainState} />
+            </Tab>
+            <Tab eventKey="cash" title="Cash">
+              <CashTab blockchainState={blockchainState} />
+            </Tab>
+            <Tab eventKey="Auditors" title="Auditors">
+              <AuditorsTab blockchainState={blockchainState} />
+            </Tab>
+          </Tabs>
+        </div>
       </div>
     </div>
   );
