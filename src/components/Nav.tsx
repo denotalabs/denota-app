66bfd4982c18ccfe0bacff70d012385dd17d3a32
Mohamed Basrai <mohamedalibasrai@gmail.com>
2022-11-13 17:34:52 -0500

    initial nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,9 @@ import {
   Stack,
   Text,
   useColorMode,
+  ButtonGroup,
 } from "@chakra-ui/react";
+import Link from "next/link";
 
 import NavbarUser from "./NavbarUser";
 import SettingsCog from "./SettingsCog";
@@ -24,7 +26,20 @@ export default function Nav(props: Props) {
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Text as="b">CRXProtocol</Text>
+          <Stack direction={"row"} spacing={6}>
+            <Text as="b">CRXProtocol</Text>
+            <ButtonGroup variant="link" spacing="6">
+              <Link href="/" passHref>
+                <Button key="dashboard">Dashboard</Button>
+              </Link>
+              <Link href="/learn" passHref>
+                <Button key="docs">Docs</Button>
+              </Link>
+              <Link href="/about" passHref>
+                <Button key="about">About</Button>
+              </Link>
+            </ButtonGroup>
+          </Stack>
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
               <NavbarUser />
