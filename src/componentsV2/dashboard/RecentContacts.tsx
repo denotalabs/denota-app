f3b70063fc15090619bde869b6b76e392515ff6a
Mohamed Basrai <mohamedalibasrai@gmail.com>
2022-11-14 09:33:19 -0500

    todo

--- src/componentsV2/dashboard/RecentContacts.tsx
+++ src/componentsV2/dashboard/RecentContacts.tsx
@@ -1,6 +1,7 @@
 import { Box, Center, Text } from "@chakra-ui/react";
 
 function RecentContacts() {
+  // TODO (Integrate v2 UI with v2 smart contract): Load recent contacts from graph
   return (
     <Box bg="cadetblue" height={400} width="100%">
       <Center>
