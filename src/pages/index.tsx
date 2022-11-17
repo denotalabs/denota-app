06d93cf8bfa74f39e8d7c0a38ee14baed75ab560
Mohamed Basrai <mohamedalibasrai@gmail.com>
2022-11-17 08:11:04 -0500

    legacy flow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,17 @@
-import { useState } from "react";
-import { Heading, Stack, Text, Center } from "@chakra-ui/react";
+import { Stack, Center } from "@chakra-ui/react";
 
-import Nav from "../components/Nav";
-import UserFlow from "../components/flows/UserFlow";
-import AuditorFlow from "../components/flows/AuditorFlow";
-import { useBlockchainData } from "../context/BlockchainDataProvider";
 import HomeScreen from "../componentsV2/dashboard/HomeScreen";
 
 function HomePage() {
-  const blockchainState = useBlockchainData();
-  const [isUser, setIsUser] = useState(true);
-  const [isV2, setIsV2] = useState(true);
-  const v1Flow = isUser ? <UserFlow /> : <AuditorFlow />;
-
   return (
     <>
       <Center my={4} py={4}>
         <Stack width="100%">
-          <Center>{isV2 ? <HomeScreen /> : null}</Center>
+          <Center>
+            <HomeScreen />
+          </Center>
         </Stack>
       </Center>
-      {isV2 ? null : v1Flow}
     </>
   );
 }
