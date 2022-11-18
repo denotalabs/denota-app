d9a0390135374253ea6d9d36bed7aba2aa171bca
Mohamed Basrai <mohamedalibasrai@gmail.com>
2022-11-18 09:32:09 -0500

    load confirm details

--- src/componentsV2/write/confirm/ConfirmDetails.tsx
+++ src/componentsV2/write/confirm/ConfirmDetails.tsx
@@ -1,12 +1,23 @@
 import { Center, VStack } from "@chakra-ui/react";
+import { useMemo } from "react";
+import { useStep } from "../../stepper/Stepper";
 import ConfirmDetailsRow from "./ConfirmDetailsRow";
 
 function ConfirmDetails() {
+  const { formData } = useStep();
+  const maturityDate = useMemo(() => {
+    const date = new Date();
+    date.setDate(date.getDate() + Number(formData.inspection));
+    return date.toDateString();
+  }, [formData.inspection]);
   return (
     <VStack borderRadius={10} bg="gray.700" w="100%" p={6}>
-      <ConfirmDetailsRow title="Client address" value="0xDEADBEEF" />
-      <ConfirmDetailsRow title="Payment Amount" value="42 wETH" />
-      <ConfirmDetailsRow title="Maturity Date" value="Jan 23, 2023" />
+      <ConfirmDetailsRow title="Client address" value={formData.address} />
+      <ConfirmDetailsRow
+        title="Payment Amount"
+        value={formData.amount + " " + formData.token}
+      />
+      <ConfirmDetailsRow title="Maturity Date" value={maturityDate} />
     </VStack>
   );
 }
