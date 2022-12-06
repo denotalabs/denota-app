627f2fe9b3fd997c5fd95aef2a1fa170b6796743
Mohamed Basrai <mohamedalibasrai@gmail.com>
2022-12-06 14:52:03 -0500

    loading state

--- src/context/BlockchainDataProvider.tsx
+++ src/context/BlockchainDataProvider.tsx
@@ -125,14 +125,15 @@ export const BlockchainDataProvider = memo(
     }, []);
 
     const loadBlockchainData = useCallback(async () => {
-      const [provider, signer, account] = await connectWalletWeb3Modal(); // console.log(provider, signer, account)
-      const userType =
-        account == "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-          ? "Customer"
-          : account == "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
-          ? "Merchant"
-          : "Auditor";
+      setIsInitializing(true);
       try {
+        const [provider, signer, account] = await connectWalletWeb3Modal(); // console.log(provider, signer, account)
+        const userType =
+          account == "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+            ? "Customer"
+            : account == "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+            ? "Merchant"
+            : "Auditor";
         const { chainId } = await provider.getNetwork();
         const mapping = mappingForChainId(chainId);
 
