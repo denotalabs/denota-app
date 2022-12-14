889ba1d0fb6455bdf0dc61f514a6935c8432f844
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2022-12-14 14:09:03 -0500

    remove  from event entities, add Transaction loadin/saving, add initial working version of Write and Transfer and Cash and Fund event functions, remove broker entity event for now and handle them as separate event handlers

--- src/graph/subgraph/mapping.ts
+++ src/graph/subgraph/mapping.ts
@@ -13,17 +13,31 @@ import { ERC20,
          Cheq,
          SelfSignTimeLock, 
          BrokerRegistry } from "../subgraph/generated/schema"  // Entities that contain the event info
-import {
-        decimals,
-        events,
-        transactions,
-        } from '@amxx/graphprotocol-utils'
 
 function saveNewAccount(account: string): Account{
   let newAccount = new Account(account)
   newAccount.save()
   return newAccount
 }
+
+function saveTransaction(
+  transactionHexHash: string, 
+  cheqId: string, 
+  timestamp: BigInt, 
+  blockNumber: BigInt
+  ): Transaction {
+  // TODO not sure if the ID structure is best
+  let transaction = Transaction.load(transactionHexHash + '/' + cheqId)  // OZ Uses this entity, what to use as its ID?
+  if (transaction == null) {
+    transaction = new Transaction(transactionHexHash)
+    transaction.timestamp = timestamp
+    transaction.blockNumber = blockNumber
+    transaction.transactionHash = transactionHexHash
+    transaction.save()
+  }
+  return transaction
+}
+
 
 export function handleWrite(event: WrittenEvent): void {
   // Load event parameters
@@ -34,127 +48,200 @@ export function handleWrite(event: WrittenEvent): void {
   let drawer = event.params.drawer.toHexString()
   let recipient = event.params.recipient.toHexString()
   let broker = event.params.broker.toHexString()
+  let owner = event.params.owner.toHexString()
+  let transactionHexHash = event.transaction.hash.toHex()
 
   // Load entities if they exist, else create them
   let drawingAccount = Account.load(drawer)
   let receivingAccount = Account.load(recipient)
+  let owningAccount = Account.load(owner)
   let ERC20Token = ERC20.load(erc20)
-  if (ERC20Token==null){
+  if (ERC20Token==null) {
     ERC20Token = new ERC20(erc20)
     ERC20Token.save()
   }
   drawingAccount = drawingAccount == null ? saveNewAccount(drawer) : drawingAccount
   receivingAccount = receivingAccount == null ? saveNewAccount(recipient) : receivingAccount
+  owningAccount = owningAccount == null ? saveNewAccount(owner) : owningAccount
 
-  let newEscrow = new Escrow(events.id(event))  // How OZ does IDs entities that implements Event
-  newEscrow.cheq = event.params.cheqId.toString()
-  newEscrow.timestamp = event.block.timestamp
-  newEscrow.from = escrowed == BigInt.zero() ? receivingAccount.id : drawingAccount.id  // Shouldnt this depend on module?
-  newEscrow.amount = event.params.escrowed
-  newEscrow.transaction = transactions.log(event).id
-
-  // Create new cheq
   let cheq = new Cheq(cheqId)
   cheq.createdAt = event.block.timestamp
-  // cheq.transactionHash = event.block.hash.toHexString()
-  // cheq.ercToken = ERC20Token.id
-  cheq.amount = amount
-  cheq.escrowed = escrowed
+  cheq.erc20 = ERC20Token.id
+  cheq.amount = amount.divDecimal(BigInt.fromI32(18).toBigDecimal())  // TODO save the erc20's decimals by querying it instead of assuming
+  cheq.amountExact = amount
   cheq.drawer = drawingAccount.id
-  // token.owner = receivingAccount.id // TODO inefficient to add ownership info on Transfer(address(0), to, cheqId) event?
   cheq.recipient = receivingAccount.id
   cheq.broker = broker
+  cheq.uri = ""  // TODO Add URI here
+  cheq.escrowed = escrowed.divDecimal(BigInt.fromI32(18).toBigDecimal())
+  cheq.escrowedExact = escrowed
+  cheq.owner = owningAccount.id // TODO inefficient to add ownership info on Transfer(address(0), to, cheqId) event?
   cheq.save()
 
-  let brokerRegistration = BrokerRegistry.load(broker)  // How to fetch correct broker module?
-  if (brokerRegistration==null){
-    brokerRegistration = new BrokerRegistry(broker)
-    brokerRegistration.save()
-  }
-  // Increment broker's token count
-  let brokerModule = SelfSignTimeLock.load(brokerRegistration.module)
-  if (brokerModule == null){
-    brokerModule = new SelfSignTimeLock(broker)
-    brokerModule.save()
-  }
-  brokerModule.numCheqsManaged = brokerModule.numCheqsManaged.plus(BigInt.fromI32(1))
-  brokerModule.save()
-  // Increment each Account's token counts
-  drawingAccount.numCheqsSent = drawingAccount.numCheqsSent.plus(BigInt.fromI32(1))
-  drawingAccount.save()
-  // Increment each Account's token counts
-  receivingAccount.numCheqsReceived = receivingAccount.numCheqsReceived.plus(BigInt.fromI32(1))
-  receivingAccount.save()
+  let escrow = new Escrow(transactionHexHash + '/' + cheqId)  // How OZ does IDs entities that implements Event?
+  escrow.emitter = drawingAccount.id
+  escrow.cheq = event.params.cheqId.toString()
+  escrow.timestamp = event.block.timestamp
+  escrow.from = drawingAccount.id  // TODO Shouldnt this depend on module?
+  escrow.amount = event.params.escrowed
+  escrow.transaction = transactionHexHash  // TODO How OZ does it, how does it work?
+  // transaction.events.concat(event)  // TODO How does OZ do this? Need to add TransferEvent & FundedEvent
+
+  let transaction = saveTransaction(transactionHexHash, cheqId, event.block.timestamp, event.block.number)
+
+  // TODO Let modules emit their own events and update them from there
+  // let brokerModule = fetchModule(broker)
+  // brokerModule.numCheqsManaged = brokerModule.numCheqsManaged.plus(BigInt.fromI32(1))
+  // brokerModule.save()
+  // // Increment each Account's token counts
+  // drawingAccount.numCheqsSent = drawingAccount.numCheqsSent.plus(BigInt.fromI32(1))
+  // drawingAccount.save()
+  // // Increment each Account's token counts
+  // receivingAccount.numCheqsReceived = receivingAccount.numCheqsReceived.plus(BigInt.fromI32(1))
+  // receivingAccount.save()
 }
 
-export function handleTransfer(event: TransferEvent): void {  // Need to save all transfer events in Transfer table  
+export function handleTransfer(event: TransferEvent): void { 
+  // Load event params
   let from = event.params.from.toHexString()
   let to = event.params.to.toHexString()
-  let tokenId = event.params.tokenId.toHexString()
+  let cheqId = event.params.tokenId.toHexString()
+  let transactionHexHash = event.transaction.hash.toHex()
 
   // Load from and to Accounts
   let fromAccount = Account.load(from)  // Check if from is address(0) since this represents mint()
   let toAccount = Account.load(to)
-  let token = Token.load(tokenId)  // Write event fires before Transfer event: token should exist??
-  if (token == null){  // SHOULDN'T BE THE CASE?
-    token = new Token(tokenId)
-    token.save()
-  }
+  fromAccount = fromAccount == null ? saveNewAccount(from) : fromAccount
   toAccount = toAccount == null ? saveNewAccount(to) : toAccount
-  toAccount.numTokensOwned = toAccount.numTokensOwned.plus(BigInt.fromI32(1))
+
+  // Load Cheq
+  let cheq = Cheq.load(cheqId)  // Write event fires before Transfer event: cheq should exist
+  if (cheq == null){  // SHOULDN'T BE THE CASE
+    cheq = new Cheq(cheqId)
+    cheq.save()
+  }
+
+  // Update accounts' cheq balances
+  if (event.params.from != Address.zero()){ 
+    fromAccount.numCheqsOwned = fromAccount.numCheqsSent.minus(BigInt.fromI32(1))
+    fromAccount.save()
+  }
+  toAccount.numCheqsOwned = toAccount.numCheqsOwned.plus(BigInt.fromI32(1))
   toAccount.save()
 
-  fromAccount = fromAccount == null ? saveNewAccount(from) : fromAccount 
-  if (from != "0x0000000000000000000000000000000000000000"){  // If minting, don't subtract from address(0)'s tokens owned
-    // Decrement from Account's token ownership and increment to's
-    fromAccount.numTokensOwned = fromAccount.numTokensSent.minus(BigInt.fromI32(1))
-  }
-  fromAccount.save()
+  let transaction = saveTransaction(transactionHexHash, cheqId, event.block.timestamp, event.block.number)
 
-  let newOwnership = new Ownership(event.block.timestamp.toString())
-  newOwnership.tokenId = BigInt.fromI32(0)
-  newOwnership.from = fromAccount.id
-  newOwnership.to = toAccount.id
-  newOwnership.save()
+  let transfer = new Transfer(transactionHexHash + '/' + cheqId)
+  transfer.emitter = fromAccount.id
+  transfer.transaction = transactionHexHash
+  transfer.timestamp = event.block.timestamp
+  transfer.cheq = cheqId
+  transfer.from = fromAccount.id
+  transfer.to = toAccount.id
+  transfer.save()
 }
 
 export function handleFund(event: FundedEvent): void {  // Save in Escrow table
-  let token = Token.load(event.params.cheqId.toHexString())
+  // Load event params
   let fromAccount = Account.load(event.params.from.toHexString())
+  fromAccount = fromAccount == null ? saveNewAccount(event.params.from.toHexString()) : fromAccount
   let amount = event.params.amount
+  let transactionHexHash = event.transaction.hash.toHex()
+  let cheqId = event.params.cheqId.toString()
 
-  fromAccount = fromAccount==null ? saveNewAccount(event.params.from.toHexString()) : fromAccount
-  if (token!=null){
-  //   ownerAccount.tokensCashed = ownerAccount.tokensCashed.concat([token.id])
-  //   ownerAccount.numTokensCashed = ownerAccount.numTokensCashed.plus(BigInt.fromI32(1))
-  //   token.save()
-  //   ownerAccount.save()
-  } else{ // SHOULDN'T HAPPEN?
+  // Load cheq
+  let cheq = Cheq.load(cheqId)
+  if (cheq == null){  // SHOULDN'T BE THE CASE
+    cheq = new Cheq(cheqId)
+    cheq.save()
   }
+  let transaction = saveTransaction(transactionHexHash, cheqId, event.block.timestamp, event.block.number)
 
-  let newEscrow = new Escrow(event.block.timestamp.toString())
-  newEscrow.caller = fromAccount.id
-  newEscrow.tokenId = event.params.cheqId
-  newEscrow.amount = amount
+  let escrow = new Escrow(transactionHexHash + '/' + cheqId)
+  escrow.emitter = fromAccount.id
+  escrow.transaction = transactionHexHash
+  escrow.timestamp = event.block.timestamp
+  escrow.cheq = cheqId
+  escrow.from = fromAccount.id
+  escrow.amount = amount
 }
 
 export function handleCash(event: CashedEvent): void {
-  let token = Token.load(event.params.cheqId.toHexString())
-  let ownerAccount = Account.load(event.params.to.toHexString())
-  ownerAccount = ownerAccount==null ? saveNewAccount(event.params.to.toHexString()) : ownerAccount
-  if (token!=null){
-    token.save()
-    ownerAccount.save()
-  } else{ // SHOULDN'T HAPPEN?
-  }
+    // Load event params
+    let toAccount = Account.load(event.params.to.toHexString())
+    toAccount = toAccount == null ? saveNewAccount(event.params.to.toHexString()) : toAccount
+    let amount = event.params.amount
+    let transactionHexHash = event.transaction.hash.toHex()
+    let cheqId = event.params.cheqId.toString()
+  
+    // Load cheq
+    let cheq = Cheq.load(cheqId)
+    if (cheq == null){  // SHOULDN'T BE THE CASE
+      cheq = new Cheq(cheqId)
+      cheq.save()
+    }
 
-  // let newEscrow = new Escrow(event.block.timestamp.toString())
-  // newEscrow.caller = escrowed == BigInt.zero() ? receivingAccount.id : drawingAccount.id
-  // newEscrow.tokenId = event.params.cheqId
-  // newEscrow.amount = event.params.escrowed
+    // Load transaction
+    let transaction = saveTransaction(transactionHexHash, cheqId, event.block.timestamp, event.block.number)
+  
+    let escrow = new Escrow(transactionHexHash + '/' + cheqId)
+    escrow.emitter = toAccount.id
+    escrow.transaction = transactionHexHash
+    escrow.timestamp = event.block.timestamp
+    escrow.cheq = cheqId
+    escrow.from = toAccount.id
+    escrow.amount = amount.neg()  // TODO may need more general differentiation of Cashing/Funding
 }
-export function handleWhitelist(event: BrokerWhitelisted): void {
-  let broker = event.params.broker
-  let isAccepted = event.params.isAccepted
-  let brokerName = event.params.brokerName
-}
+
+// export function handleWhitelist(event: BrokerWhitelisted): void {
+//   let broker = event.params.broker
+//   let isAccepted = event.params.isAccepted
+//   let brokerName = event.params.brokerName
+// }
+
+// // let loadSSTL = (moduleAddress: string) => {
+// //   let brokerModule = SelfSignTimeLock.load(moduleAddress)
+// //     if (brokerModule == null){
+// //       brokerModule = new SelfSignTimeLock(moduleAddress)
+// //       brokerModule.save()
+// //     }
+// //     return brokerModule
+// // }
+// // let moduleSwitch = {"SelfSignTimeLock": loadSSTL}
+
+// function fetchModule(moduleAddress: string) : SelfSignTimeLock {
+//   let brokerRegistration = BrokerRegistry.load(moduleAddress)  // How to fetch correct broker module?
+//   if (brokerRegistration==null){
+//     brokerRegistration = new BrokerRegistry(moduleAddress)
+//     brokerRegistration.save()
+//   }
+//   // Replace string with brokerRegistration.name
+//   // let brokerModule = moduleSwitch["SelfSignTimeLock"](brokerRegistration.module)
+//   let brokerModule = SelfSignTimeLock.load(brokerRegistration.module)
+//     if (brokerModule == null){
+//       brokerModule = new SelfSignTimeLock(moduleAddress)
+//       brokerModule.save()
+//     }
+//   return brokerModule
+//   // if (moduleAddress == "SelfSignTimeLock") {
+//   //   let brokerModule = SelfSignTimeLock.load(brokerRegistration.module)
+//   //   if (brokerModule == null){
+//   //     brokerModule = new SelfSignTimeLock(moduleAddress)
+//   //     brokerModule.save()
+//   //   }
+//   //   return SelfSignTimeLock
+//   // } else if (moduleAddress == "Bank") {
+//   // } else {
+//   // }
+// }
+// function updateModule() : void {
+//   // cheqsManaged: [Cheq!]! @derivedFrom(field: "broker")
+//   // numCheqsManaged: BigInt!
+//   // cheqFunder: Account!
+//   // cheqReceiver: Account!
+//   // cheqCreated: BigInt!  # Is redundant?
+//   // cheqInspectionPeriod: BigInt!
+//   // isEarlyReleased: Boolean!
+//   // earlyReleasedTime: BigInt
+//   // cheqWhitelist: [ERC20!]!
+// }
