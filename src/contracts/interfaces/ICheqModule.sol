ec92764d936bd8dbdc4eb19d55fe7be519d83ba7
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2023-01-31 09:33:08 -0600

    add both flat and percent fees on fund and write (can send empty and fund for free otherwise), add additional return values for ICheqModule hooks including uint for the module fee in WFC and address for transfer/approval logic outsourced to the module, added moduleTokenRevenue so modules can withdraw their revenue from CheqRegistrar

--- src/contracts/interfaces/ICheqModule.sol
+++ src/contracts/interfaces/ICheqModule.sol
@@ -4,11 +4,11 @@ import {DataTypes} from "../libraries/DataTypes.sol";
 
 // Question: Should the require statements be part of the interface? Would allow people to query canWrite(), canCash(), etc
 interface ICheqModule {
-    function processWrite(address caller, address owner, uint cheqId, DataTypes.Cheq calldata cheq, bytes calldata initData) external returns (bool);
-    function processTransfer(address caller, address from, address to, uint256 cheqId, DataTypes.Cheq calldata cheq, bytes memory data) external returns (bool);
-    function processFund(address caller, uint256 amount, uint256 cheqId, DataTypes.Cheq calldata cheq, bytes calldata initData) external returns (bool);
-    function processCash(address caller, address to, uint256 amount, uint256 cheqId, DataTypes.Cheq calldata cheq, bytes calldata initData) external returns (bool);
-    function processApproval(address caller, address to, uint256 cheqId, DataTypes.Cheq calldata cheq, bytes memory initData) external returns (bool);
+    function processWrite(address caller, address owner, uint cheqId, DataTypes.Cheq calldata cheq, bytes calldata initData) external returns (bool, uint256);
+    function processTransfer(address caller, address from, address to, uint256 cheqId, DataTypes.Cheq calldata cheq, bytes memory data) external returns (bool, address);
+    function processFund(address caller, uint256 amount, uint256 cheqId, DataTypes.Cheq calldata cheq, bytes calldata initData) external returns (bool, uint256);
+    function processCash(address caller, address to, uint256 amount, uint256 cheqId, DataTypes.Cheq calldata cheq, bytes calldata initData) external returns (bool, uint256);
+    function processApproval(address caller, address to, uint256 cheqId, DataTypes.Cheq calldata cheq, bytes memory initData) external returns (bool, address);
     // function processOwnerOf(address owner, uint256 tokenId) external view returns(bool); // TODO settle on what this returns
     // function processBalanceOf() external view returns(uint256);
 }
