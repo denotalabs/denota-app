960e8da7d4221d3de0036f42e071c338b70eed5d
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-02-06 16:42:19 -0600

    update module screen

--- src/components/write/module/ModuleInfo.tsx
+++ src/components/write/module/ModuleInfo.tsx
@@ -22,6 +22,8 @@ function ModuleInfo() {
   const [sliderValue, setSliderValue] = useState(100);
   const [showTooltip, setShowTooltip] = useState(false);
 
+  const [value, setValue] = useState("1");
+
   return (
     <RoundedBox padding={6}>
       <Flex flexWrap={"wrap"} gap={"18px"} direction={"column"}>
@@ -38,11 +40,11 @@ function ModuleInfo() {
           </FormLabel>
           <Inspection />
         </FormControl>
-        <FormControl>
+        <FormControl mt={3}>
           <FormLabel noOfLines={1} flexShrink={0}>
             Down payment amount
             <Tooltip
-              label="The amount of time the payer has to request a refund"
+              label="The minimum payment required to start work"
               aria-label="module tooltip"
               placement="right"
             >
@@ -54,10 +56,11 @@ function ModuleInfo() {
             defaultValue={100}
             min={0}
             max={100}
-            colorScheme="teal"
             onChange={(v) => setSliderValue(v)}
             onMouseEnter={() => setShowTooltip(true)}
             onMouseLeave={() => setShowTooltip(false)}
+            color="brand.200"
+            ringColor="brand.200"
             w="80%"
           >
             <SliderMark value={0} mt="1" fontSize="sm">
@@ -69,12 +72,12 @@ function ModuleInfo() {
             <SliderMark value={100} mt="1" ml="-2.5" fontSize="sm">
               100%
             </SliderMark>
-            <SliderTrack>
-              <SliderFilledTrack />
+            <SliderTrack bg="brand.300">
+              <SliderFilledTrack bg="brand.200" />
             </SliderTrack>
             <Tooltip
               hasArrow
-              bg="teal.500"
+              bg="brand.200"
               color="white"
               placement="top"
               isOpen={showTooltip}
@@ -88,17 +91,36 @@ function ModuleInfo() {
           <FormLabel noOfLines={1} flexShrink={0}>
             Disputation method
             <Tooltip
-              label="The amount of time the payer has to request a refund"
+              label="Method for resolving payment disputes"
               aria-label="module tooltip"
               placement="right"
             >
               <QuestionOutlineIcon ml={2} mb={1} />
             </Tooltip>
-            <RadioGroup defaultValue="1" ml="2" mt="2">
+            <RadioGroup onChange={setValue} value={value} ml="2" mt="2">
               <Stack direction="column">
-                <Radio value="1">Self-serve</Radio>
-                <Radio value="2">Third-party auditor</Radio>
-                <Radio value="3">Kleros</Radio>
+                {/*TODO: figure out correct way to set radio colors*/}
+                <Radio
+                  colorScheme="brand.200"
+                  bgColor={value == "1" ? "brand.200" : "clear"}
+                  value="1"
+                >
+                  Self-serve
+                </Radio>
+                <Radio
+                  colorScheme="brand.200"
+                  bgColor={value == "2" ? "brand.200" : "clear"}
+                  value="2"
+                >
+                  Third-party auditor
+                </Radio>
+                <Radio
+                  colorScheme="brand.200"
+                  bgColor={value == "3" ? "brand.200" : "clear"}
+                  value="3"
+                >
+                  Kleros
+                </Radio>
               </Stack>
             </RadioGroup>
           </FormLabel>
@@ -107,7 +129,7 @@ function ModuleInfo() {
           <FormLabel noOfLines={1} flexShrink={0}>
             Milestones
             <Tooltip
-              label="The amount of time the payer has to request a refund"
+              label="Break the payment into milestones"
               aria-label="module tooltip"
               placement="right"
             >
@@ -116,57 +138,14 @@ function ModuleInfo() {
           </FormLabel>
           <IconButton
             variant="outline"
-            colorScheme="teal"
             aria-label="Call Sage"
             size="sm"
             fontSize="15px"
+            color="brand.200"
             icon={<AddIcon />}
           />
         </FormControl>
       </Flex>
-
-      {/* <Grid
-        templateColumns="repeat(auto-fit, minmax(240px, 1fr))"
-        templateRows="repeat(1, 1fr)"
-        gap={6}
-        h="100%"
-      >
-        <GridItem>
-          <Flex alignItems={"center"}>
-            <FormControl>
-              <FormLabel>
-                Module
-                <Tooltip
-                  label="The module specifies the payment conditions"
-                  aria-label="module tooltip"
-                  placement="right"
-                >
-                  <QuestionOutlineIcon ml={2} mb={1} />
-                </Tooltip>
-              </FormLabel>
-
-              <ModuleSelect />
-            </FormControl>
-          </Flex>
-        </GridItem>
-        <GridItem>
-          <Flex alignItems={"center"}>
-            <FormControl>
-              <FormLabel noOfLines={1} flexShrink={0}>
-                Inspection Period
-                <Tooltip
-                  label="The amount of time the payer has to request a refund"
-                  aria-label="module tooltip"
-                  placement="right"
-                >
-                  <QuestionOutlineIcon ml={2} mb={1} />
-                </Tooltip>
-              </FormLabel>
-              <Inspection />
-            </FormControl>
-          </Flex>
-        </GridItem>
-      </Grid> */}
     </RoundedBox>
   );
 }
