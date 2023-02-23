93031cb1d0d80c759a13f66464a7c7c25b7b324c
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-02-23 15:09:00 -0500

    update url

--- src/hooks/useCheqs.tsx
+++ src/hooks/useCheqs.tsx
@@ -2,10 +2,7 @@ import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 import { BigNumber } from "ethers";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { CheqCurrency } from "../components/designSystem/CurrencyIcon";
-import {
-  APIURL_LOCAL,
-  useBlockchainData,
-} from "../context/BlockchainDataProvider";
+import { APIURL, useBlockchainData } from "../context/BlockchainDataProvider";
 
 interface Props {
   cheqField: string;
@@ -201,7 +198,7 @@ export const useCheqs = ({ cheqField }: Props) => {
 
       // SWITCH BACK TO PROD URL BEFORE MERGE
       const client = new ApolloClient({
-        uri: APIURL_LOCAL,
+        uri: APIURL,
         cache: new InMemoryCache(),
       });
       client
