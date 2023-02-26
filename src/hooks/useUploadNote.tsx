2832326ea4b4d1151eb6b2b5d7558337abc0b1ff
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-02-26 11:49:18 -0700

    cleanup

--- src/hooks/useUploadNote.tsx
+++ src/hooks/useUploadNote.tsx
@@ -5,22 +5,7 @@ const CHEQ_NOTE_SERVICE_URL_LOCAL = "http://127.0.0.1:3001/";
 
 const CHEQ_NOTE_SERVICE = "https://klymr.me/nft";
 
-const CHEQ_FILE_SERVICE = "http://127.0.0.1:3001/";
-
 export const useUploadNote = () => {
-  const uploadNote = useCallback(async (note: string) => {
-    try {
-      const resp = await axios.post(CHEQ_NOTE_SERVICE, {
-        name: "Cheq",
-        description: note,
-        mode: "S3",
-      });
-      return resp.data.key as string;
-    } catch {
-      return undefined;
-    }
-  }, []);
-
   const uploadFile = useCallback(async (file: any, note: string) => {
     if (!file && !note) {
       return;
@@ -57,5 +42,5 @@ export const useUploadNote = () => {
     }
   }, []);
 
-  return { uploadNote, uploadFile };
+  return { uploadFile };
 };
