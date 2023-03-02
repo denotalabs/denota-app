aeab9c313916e5975dff087f8c77be3546ad0204
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-03-02 11:46:05 -0700

    update text

--- src/send_email/send_email.mjs
+++ src/send_email/send_email.mjs
@@ -12,17 +12,17 @@ app.post("/send-email", jsonParser, async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "your_email@gmail.com",
-      pass: "your_password",
+      user: "denotatest@gmail.com",
+      pass: process.env.GMAIL_KEY,
     },
   });
 
   // Create an email object with the HTML content and recipient
   const mailOptions = {
-    from: "your_email@gmail.com",
-    to: "recipient_email@example.com",
-    subject: "Hello from Node.js!",
-    html: "<h1>Welcome to my Node.js app!</h1><p>This is an HTML email sent from a Node.js app using Nodemailer and Express.js.</p>",
+    from: "denotatest@gmail.com",
+    to: req.body.email,
+    subject: "Hola from Denota",
+    html: "<h1>You've received a Nota!</h1><p>Visit denota.xyz to view it</p>",
   };
 
   // Use the transport object to send the email
