8280d5e50828391c17168fe5aa97424d53de9b61
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-03-01 22:58:50 -0700

    add comment

--- src/context/providerOptions.ts
+++ src/context/providerOptions.ts
@@ -1,5 +1,6 @@
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 
+// TODO: Add back wallet connect after setting up Infura account
 export const providerOptions = {
   walletlink: {
     package: CoinbaseWalletSDK, // Required
