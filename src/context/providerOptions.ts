6e634120d4203bc6ff2453d207dfb2d2e9f70158
soolaymahn <mohamedalibasrai@gmail.com>
2023-03-02 00:06:36 -0600

    Remove wallet connect until we set up infura  (#113)
    * remove wallet connect
    
    * add comment

--- src/context/providerOptions.ts
+++ src/context/providerOptions.ts
@@ -1,18 +1,12 @@
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
-import WalletConnectProvider from "@walletconnect/web3-provider";
 
+// TODO: Add back wallet connect after setting up Infura account
 export const providerOptions = {
   walletlink: {
     package: CoinbaseWalletSDK, // Required
     options: {
       appName: "CheqProtocol", // Required
       // infuraId: process.env.INFURA_KEY
-    },
-  },
-  walletconnect: {
-    package: WalletConnectProvider, // required
-    options: {
-      infuraId: "", // required
     },
   },
 };
