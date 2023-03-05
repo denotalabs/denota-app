dc2cf7ceafa8db4afd90af752cf0b3816791fa55
soolaymahn <mohamedalibasrai@gmail.com>
2023-03-04 20:45:00 -0600

    Add back wallet connect (#128)
    * add back wallet connect
    
    * repush

--- src/context/providerOptions.ts
+++ src/context/providerOptions.ts
@@ -1,4 +1,5 @@
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 // TODO: Add back wallet connect after setting up Infura account
 export const providerOptions = {
@@ -7,6 +8,12 @@ export const providerOptions = {
     options: {
       appName: "CheqProtocol", // Required
       // infuraId: process.env.INFURA_KEY
+    },
+  },
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: process.env.NEXT_PUBLIC_INFURA_KEY,
     },
   },
 };
