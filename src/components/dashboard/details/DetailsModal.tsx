cbeb41396041a6fa7ced45dd63e0c7438a7cf46f
Rafi <rafi337161@gmail.com>
2023-03-04 19:33:48 -0700

    saving work

--- src/components/dashboard/details/DetailsModal.tsx
+++ src/components/dashboard/details/DetailsModal.tsx
@@ -1,6 +1,8 @@
+import { Center } from "@chakra-ui/react";
 import { Cheq } from "../../../hooks/useCheqs";
 import SimpleModal from "../../designSystem/SimpleModal";
 import CheqDetails from "./CheqDetails";
+import ShareToLensButton from "./ShareToLensButton";
 
 interface Props {
   isOpen: boolean;
@@ -12,6 +14,9 @@ function DetailsModal(props: Props) {
   return (
     <SimpleModal {...props}>
       <CheqDetails cheq={props.cheq} />
+      <Center>
+        <ShareToLensButton text="hello world" url="https://mycoolapp.xyz" />
+      </Center>
     </SimpleModal>
   );
 }
