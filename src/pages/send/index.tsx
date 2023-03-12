c0abf8865e32e062ba78c6f01f71570968257e21
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-03-12 19:55:55 -0400

    comment

--- src/pages/send/index.tsx
+++ src/pages/send/index.tsx
@@ -5,9 +5,6 @@ function SendPage() {
   return (
     <Center w="100%" h="100%">
       <WriteCheqFlow isInvoice={true} />
-
-      {/* <VStack w="650px" bg="brand.100" py={2} px={4} borderRadius="30px">
-      </VStack> */}
     </Center>
   );
 }
