0c887e29439eb601d876bb8bc41090f39acc0597
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-03-13 11:55:48 -0400

    use update registrar address

--- src/send_email/send_email.mjs
+++ src/send_email/send_email.mjs
@@ -3,8 +3,10 @@ import { ethers } from "ethers";
 import express from "express";
 import nodemailer from "nodemailer";
 
+import chainInfo from "../../contractAddresses.json" assert { type: "json" };
+
 const app = express();
-const port = 6001;
+const port = 3001;
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,8 +18,6 @@ app.use(function (req, res, next) {
 });
 
 var jsonParser = bodyParser.json();
-
-const registrar = "0x7338C511171c6cabf35195448921a4dD044fcef6";
 
 app.post("/", jsonParser, async (req, res) => {
   try {
@@ -32,15 +32,19 @@ app.post("/", jsonParser, async (req, res) => {
     const { email, txHash, token, amount, isInvoice, network } = req.body;
 
     let provider;
+    let registrar;
+
     if (network === "0x13881") {
       provider = new ethers.providers.JsonRpcProvider(
         "https://rpc-mumbai.maticvigil.com"
       );
+      registrar = chainInfo.mumbai.registrar;
     }
     if (network === "0xaef3") {
       provider = new ethers.providers.JsonRpcProvider(
         "https://alfajores-forno.celo-testnet.org"
       );
+      registrar = chainInfo.alfajores.registrar;
     }
 
     if (provider) {
@@ -84,5 +88,6 @@ app.post("/", jsonParser, async (req, res) => {
 });
 
 app.listen(port, function () {
+  console.log(chainInfo.mumbai.registrar);
   console.log("Server listening on port " + port);
 });
