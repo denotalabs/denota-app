59dc9abf3f904146b5ec0f63f76ffff0f1821a41
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-03-16 18:51:33 -0400

    remove fleek script

--- contracts/src/CheqRegistrar.sol
+++ contracts/src/CheqRegistrar.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.16;
-import "openzeppelin/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "openzeppelin/token/ERC20/IERC20.sol";
 import "openzeppelin/token/ERC721/ERC721.sol";
 import "openzeppelin/token/ERC20/utils/SafeERC20.sol";
