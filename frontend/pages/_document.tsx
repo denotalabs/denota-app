d71fd6da378767a16ddaae0070e87c178c3fe6ec
Rafi <rafi337161@gmail.com>
2023-03-16 19:56:53 -0500

    added goog anal

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -5,7 +5,21 @@ import customTheme from "../theme";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RX5F5Q2B8D"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RX5F5Q2B8D');
+          `}
+        </script>
+      </Head>
       <body>
         <ColorModeScript
           initialColorMode={customTheme.config.initialColorMode}
