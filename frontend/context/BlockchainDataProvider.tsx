1bcf782fb2ccaac888b88b225311645f34af02a9
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-03-18 17:35:45 -0400

    update to prod url

--- frontend/context/BlockchainDataProvider.tsx
+++ frontend/context/BlockchainDataProvider.tsx
@@ -178,7 +178,7 @@ export const BlockchainDataProvider = memo(
             cheq,
             directPayAddress: contractMapping.directPay,
             chainId: chainNumberToChainHex(chainId),
-            graphUrl: deployedChainInfo.graphTestUrl, // Change to graphTestUrl for testing a local graph node
+            graphUrl: deployedChainInfo.graphUrl, // Change to graphTestUrl for testing a local graph node
             escrowAddress: contractMapping.escrow, // TODO: deploy escrow
             nativeCurrenySymbol: deployedChainInfo.nativeCurrency?.symbol ?? "",
             walletBalance: ethers.utils.formatUnits(walletBalance),
