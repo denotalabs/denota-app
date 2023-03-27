3940fd7f8be0382f796f27e1220d39ff26848634
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-03-27 13:24:40 -0400

    cleanup

--- frontend/components/dashboard/NewUserScreen.tsx
+++ frontend/components/dashboard/NewUserScreen.tsx
@@ -5,7 +5,6 @@ function NewUserScreen() {
     <Box w="100%">
       <Center flexDirection={"column"} w="100%" px={5}>
         <Text fontSize="2xl" textAlign="center">
-          {" "}
           Hola from Denota! Connect your wallet using the top right button and
           make sure you're on a supported testnet
         </Text>
