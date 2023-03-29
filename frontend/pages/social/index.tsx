06889a50d73008ad65d3ed4e82bdee9bd7d45053
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-03-29 17:43:30 -0400

    social tab

---
+++ frontend/pages/social/index.tsx
@@ -0,0 +1,13 @@
+import { Center, VStack } from "@chakra-ui/react";
+
+function SocialPage() {
+  return (
+    <Center w="100%" h="100%">
+      <VStack mt={5} bg="brand.100" py={2} px={4} borderRadius="30px">
+        Coming soon
+      </VStack>
+    </Center>
+  );
+}
+
+export default SocialPage;
