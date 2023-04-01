3f80b68dbf61ebe1c5ae5178fcef95695c226a63
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-04-01 14:12:20 -0400

    fix type

--- frontend/components/write/module/MilestoneTerms.tsx
+++ frontend/components/write/module/MilestoneTerms.tsx
@@ -33,7 +33,7 @@ export function MilestoneTerms() {
             <FormLabel noOfLines={1} flexShrink={0}>
               Milestone #{index + 1}
             </FormLabel>
-            <Input value={item} />
+            <Input value={"0.5"} />
           </FormControl>
         );
       })}
