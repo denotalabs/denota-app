ac3171a6b00effb0a66ad0b7a6c433396d2a70b2
Rafi <rafi337161@gmail.com>
2023-04-01 19:48:11 -0300

    addressing mb comment, using usememo

--- frontend/hooks/useDemoMode.ts
+++ frontend/hooks/useDemoMode.ts
@@ -1,21 +1,21 @@
+import { useMemo } from "react";
 import { useBlockchainData } from "../context/BlockchainDataProvider";
 
 const useDemoMode = () => {
   const { blockchainState } = useBlockchainData();
   const { account } = blockchainState;
 
-  const isDemoWallet = (walletAddress: string): boolean => {
+  const isDemoWallet = useMemo(() => {
     const demoWallets = [
       "0x167813E0D6958BCF30a1cEbEE53aE0C57677c963", // Alex
       "0x374E4F9EF906F3e51df1b3305936Ec18A6797748", // Rafi
       "0x1d2CBA07B5EFE517586F0453303B5CAFa904e5ca",
-      "0x91663f613945C9F90CC2b74328Db44441D993172" // MB
-    ]; 
-    return demoWallets.includes(walletAddress);
-  };
+      "0x91663f613945C9F90CC2b74328Db44441D993172", // MB
+    ];
+    return demoWallets.includes(account);
+  }, [account]);
 
-  const isDemoMode = isDemoWallet(account);
-  return isDemoMode;
+  return isDemoWallet;
 };
 
 export default useDemoMode;
