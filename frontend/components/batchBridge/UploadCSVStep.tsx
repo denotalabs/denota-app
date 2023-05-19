9c4f7046a9bd6b873d2b632914aab6fa305b2e8d
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-05-19 10:59:11 -0400

    read CSV data

--- frontend/components/batchBridge/UploadCSVStep.tsx
+++ frontend/components/batchBridge/UploadCSVStep.tsx
@@ -1,9 +1,28 @@
-import { Text, VStack } from "@chakra-ui/react";
+import { InputGroup, Text, VStack } from "@chakra-ui/react";
+import { ChangeEvent, useRef, useState } from "react";
+import useBatchPaymentReader from "../../hooks/batch/useBatchPaymentReader";
 import RoundedButton from "../designSystem/RoundedButton";
 import { ScreenProps, useStep } from "../designSystem/stepper/Stepper";
 
 const UploadCSVStep: React.FC<ScreenProps> = () => {
   const { next } = useStep();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [fileName, setFileName] = useState<string | undefined>(undefined);
+
+  const { handleFileRead } = useBatchPaymentReader();
+
+  const handleClick = () => {
+    inputRef.current?.click();
+  };
+
+  const handleChange = async (value: ChangeEvent<HTMLInputElement>) => {
+    if (value.target.files?.[0] && value.target.files?.[0].size < 5000000) {
+      setFileName(value.target.files?.[0].name);
+      const data = await handleFileRead(value.target.files?.[0]);
+      console.log(data);
+    }
+  };
 
   return (
     <VStack w="100%" p={4}>
@@ -11,9 +30,20 @@ const UploadCSVStep: React.FC<ScreenProps> = () => {
       <Text textAlign="center" pb={5} fontWeight={600}>
         payee,amount,token,dest_chain
       </Text>
-      <RoundedButton type="submit" onClick={next}>
-        {"Upload CSV"}
-      </RoundedButton>
+      <InputGroup onClick={handleClick}>
+        <input
+          onChange={handleChange}
+          type="file"
+          accept=".csv"
+          multiple={false}
+          id={"batch"}
+          ref={inputRef}
+          hidden
+        />
+        <RoundedButton type="submit" onClick={next}>
+          {"Upload CSV"}
+        </RoundedButton>
+      </InputGroup>
     </VStack>
   );
 };
--- frontend/hooks/batch/useBatchPaymentReader.tsx
+++ frontend/hooks/batch/useBatchPaymentReader.tsx
@@ -1,5 +1,5 @@
-import Papa from 'papaparse';
-import { useState } from 'react';
+import Papa from "papaparse";
+import { useCallback, useState } from "react";
 
 interface CsvData {
   payee: string;
@@ -11,48 +11,56 @@ interface DataMap {
   [dest_chain: string]: CsvData[];
 }
 
-const useCsvReader = () => {
+const useBatchPaymentReader = () => {
   const [data, setData] = useState<DataMap>({});
 
-  const handleFileRead = (file: File) => {
-    Papa.parse(file, {
-      header: true,
-      complete: (results) => {
-        const dataMap: DataMap = {};
-        const rows = results.data as any[];
+  const handleFileRead = useCallback(async (file: File): Promise<DataMap> => {
+    return new Promise((resolve, reject) => {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          const dataMap: DataMap = {};
+          const rows = results.data as any[];
 
-        for (const row of rows) {
-          const { payee, amount, token, dest_chain } = row;
+          for (const row of rows) {
+            const { payee, amount, token, dest_chain } = row;
 
-          // Validation
-          if (typeof payee !== 'string' || typeof dest_chain !== 'string' || typeof token !== 'string') {
-            console.error("Invalid data in row:", row);
-            continue;
+            // Validation
+            if (
+              typeof payee !== "string" ||
+              typeof dest_chain !== "string" ||
+              typeof token !== "string"
+            ) {
+              reject("Invalid data in row: " + JSON.stringify(row));
+              return;
+            }
+
+            const amountNumber = parseFloat(amount);
+
+            if (isNaN(amountNumber)) {
+              reject("Invalid amount in row: " + JSON.stringify(row));
+              return;
+            }
+
+            if (!dataMap[dest_chain]) {
+              dataMap[dest_chain] = [];
+            }
+
+            dataMap[dest_chain].push({ payee, amount: amountNumber, token });
           }
 
-          const amountNumber = parseFloat(amount);
-
-          if (isNaN(amountNumber)) {
-            console.error("Invalid amount in row:", row);
-            continue;
-          }
-
-          if (!dataMap[dest_chain]) {
-            dataMap[dest_chain] = [];
-          }
-
-          dataMap[dest_chain].push({ payee, amount: amountNumber, token });
-        }
-
-        setData(dataMap);
-      },
-      error: (error) => {
-        console.error("Error:", error);
-      },
+          setData(dataMap);
+          resolve(dataMap);
+        },
+        error: (error) => {
+          console.error("Error:", error);
+          reject(error);
+        },
+      });
     });
-  };
+  }, []);
 
   return { data, handleFileRead };
 };
 
-export default useCsvReader;
+export default useBatchPaymentReader;
