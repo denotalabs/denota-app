f03eae30e544b68f0c509e595cc579bff737f502
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-05-30 11:13:06 -0400

    add recipients toggle and confirmed state

--- frontend/components/batchBridge/DisperseDetails.tsx
+++ frontend/components/batchBridge/DisperseDetails.tsx
@@ -1,5 +1,6 @@
-import { Text, VStack } from "@chakra-ui/react";
-import { useMemo } from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
+import { Button, Text, VStack } from "@chakra-ui/react";
+import { useMemo, useState } from "react";
 import { useBlockchainData } from "../../context/BlockchainDataProvider";
 import {
   chainInfoForChainId,
@@ -24,31 +25,65 @@ function DisperseDetails({ chainId }: Props) {
     return chainInfoForChainId(chainId).displayName;
   }, [chainId]);
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  const buttonTitle = useMemo(() => {
+    if (isConfirmed) {
+      return "Confirmed";
+    }
+    if (!isCorrectChain) {
+      return;
+    }
+    return isCorrectChain ? "Confirm" : `Switch to ${chainName}`;
+  }, [chainName, isConfirmed, isCorrectChain]);
+
   return (
-    <VStack w="100%">
-      <RoundedBox mb={5} padding={6}>
+    <VStack w="100%" bg="brand.600" borderRadius="md" pt={6}>
+      <RoundedBox mb={5} px={6}>
         <Text fontWeight={600} fontSize={"lg"} textAlign="center">
           You dispersing 5000 USDC and 1000 BOB on {chainName}
         </Text>
       </RoundedBox>
+      <Button
+        mt={4}
+        leftIcon={isOpen ? <ChevronDownIcon /> : <ChevronUpIcon />}
+        onClick={() => setIsOpen(!isOpen)}
+        bg="transparent"
+        sx={{
+          "&:hover": {
+            bg: "transparent",
+          },
+        }}
+      >
+        Recipients
+      </Button>
 
-      <RoundedBox p={6}>
-        <VStack>
-          <DetailsRow title="0x123..456" value="10 USDC" />
-          <DetailsRow title="0x123..456" value="10 USDC" />
-        </VStack>
-      </RoundedBox>
+      {isOpen && (
+        <RoundedBox px={6}>
+          <VStack>
+            <DetailsRow title="0x123..456" value="10 USDC" />
+            <DetailsRow title="0x123..456" value="10 USDC" />
+          </VStack>
+        </RoundedBox>
+      )}
 
       <RoundedButton
         mt={2}
         type="submit"
+        isDisabled={isConfirmed}
         onClick={async () => {
-          await switchNetwork(chainNumberToChainHex(chainId));
-          // Force reload chain
-          connectWallet?.();
+          if (!isCorrectChain) {
+            await switchNetwork(chainNumberToChainHex(chainId));
+            // Force reload chain
+            connectWallet?.();
+          } else {
+            setIsConfirmed(true);
+          }
         }}
       >
-        {isCorrectChain ? "Confirm" : `Switch to ${chainName}`}
+        {buttonTitle}
       </RoundedButton>
     </VStack>
   );
