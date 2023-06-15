02d0d2d44fcc17339b9203f5e556acb82e97fd1f
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-06-14 23:48:25 -0400

    Add jumper link to bridge card

--- frontend/components/batchBridge/BridgeCard.tsx
+++ frontend/components/batchBridge/BridgeCard.tsx
@@ -1,12 +1,29 @@
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Text, VStack } from "@chakra-ui/react";
+import { useMemo, useState } from "react";
+import { useBlockchainData } from "../../context/BlockchainDataProvider";
+import { useTokens } from "../../hooks/useTokens";
 
 interface Props {
   chainDisplayName: string;
   token: string;
   amount: number;
+  chainId: number;
 }
 
-function BridgeCard({ chainDisplayName, token, amount }: Props) {
+function BridgeCard({ chainDisplayName, token, amount, chainId }: Props) {
+  const { getTokenAddress } = useTokens();
+  const { blockchainState } = useBlockchainData();
+
+  const tokenAddress = useMemo(
+    () => getTokenAddress(token),
+    [getTokenAddress, token]
+  );
+
+  // TODO: figure out token address on dest chain
+  const jumperLink = `https://jumper.exchange/?fromAmount=${amount}&fromChain=${blockchainState.chainId}&fromToken=${tokenAddress}&toChain=${chainId}&toToken=${tokenAddress}`;
+
+  const [wasOpened, setWasOpened] = useState(false);
+
   return (
     <Box
       w="285px"
@@ -30,6 +47,19 @@ function BridgeCard({ chainDisplayName, token, amount }: Props) {
             ? "(Lifi, 10 USDC fee, ~15 min)"
             : "(Squid, 15 USDT fee, ~20 min)"}
         </Text>
+        {wasOpened && <Text fontSize="sm">You opened this link</Text>}
+        <Button
+          variant="outline"
+          w="min(40vw, 100px)"
+          borderRadius={5}
+          colorScheme="white"
+          onClick={() => {
+            setWasOpened(true);
+            window.open(jumperLink, "_blank");
+          }}
+        >
+          Bridge now
+        </Button>
       </VStack>
     </Box>
   );
