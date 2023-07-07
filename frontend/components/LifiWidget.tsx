2fc0cb7a3f75e277cb56c1e530814a92eed0d91e
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-07-07 15:06:12 -0400

    update widget config

--- frontend/components/LifiWidget.tsx
+++ frontend/components/LifiWidget.tsx
@@ -1,15 +1,41 @@
-import { LiFiWidget } from "@lifi/widget";
+import {
+  HiddenUI,
+  LiFiWidget,
+  WidgetConfig,
+  WidgetVariant,
+} from "@lifi/widget";
+import { useMemo } from "react";
+import { useBlockchainData } from "../context/BlockchainDataProvider";
 
-export const Widget = () => {
-  return (
-    <LiFiWidget
-      config={{
-        containerStyle: {
-          border: `1px solid rgb(234, 234, 234)`,
-          borderRadius: "16px",
+export const LifiWidget = () => {
+  const { blockchainState } = useBlockchainData();
+
+  const widgetConfig: WidgetConfig = useMemo((): WidgetConfig => {
+    return {
+      variant: "expandable" as WidgetVariant,
+      subvariant: "default",
+      walletManagement: {
+        signer: blockchainState.signer,
+        connect: async () => {
+          return blockchainState.signer;
         },
-      }}
-      integrator="nextjs-example"
-    />
-  );
+        disconnect: async () => {
+          console.log("disconnect");
+        },
+      },
+      containerStyle: {
+        borderRadius: "12px",
+        boxShadow:
+          "0px 2px 4px rgba(0, 0, 0, 0.08), 0px 8px 16px rgba(0, 0, 0, 0.16)",
+      },
+      appearance: "dark",
+      hiddenUI: [HiddenUI.Appearance, HiddenUI.Language, HiddenUI.PoweredBy],
+      keyPrefix: `denota`,
+      buildUrl: true,
+      insurance: true,
+      integrator: "denota",
+    };
+  }, [blockchainState.signer]);
+
+  return <LiFiWidget config={widgetConfig} integrator="denota" />;
 };
