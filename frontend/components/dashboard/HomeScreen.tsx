57f56b69ce2a070a998c69fab57f6f114c4d9de0
Mohamed Basrai <mohamedalibasrai@gmail.com>
2023-10-01 22:51:26 -0400

    cleanup

--- frontend/components/dashboard/HomeScreen.tsx
+++ frontend/components/dashboard/HomeScreen.tsx
@@ -1,17 +1,14 @@
 import { Center } from "@chakra-ui/react";
 
-import ProtectedPage from "../ProtectedPage";
 import MyNotas from "./MyNotas";
 
 function HomeScreen() {
   return (
-    <ProtectedPage>
-      <Center alignItems={"flex-start"} width="100%" maxWidth="80rem">
-        <Center flexDirection={"column"} width="100%">
-          <MyNotas />
-        </Center>
+    <Center alignItems={"flex-start"} width="100%" maxWidth="80rem">
+      <Center flexDirection={"column"} width="100%">
+        <MyNotas />
       </Center>
-    </ProtectedPage>
+    </Center>
   );
 }
 
--- frontend/pages/send/index.tsx
+++ frontend/pages/send/index.tsx
@@ -1,14 +1,11 @@
 import { Center } from "@chakra-ui/react";
 import { TransactionCoverageFlow } from "../../components/onramps/transactions/TransactionCoverageFlow";
-import ProtectedPage from "../../components/ProtectedPage";
 
 function SendPage() {
   return (
-    <ProtectedPage>
-      <Center w="100%" h="100%">
-        <TransactionCoverageFlow />
-      </Center>
-    </ProtectedPage>
+    <Center w="100%" h="100%">
+      <TransactionCoverageFlow />
+    </Center>
   );
 }
 
