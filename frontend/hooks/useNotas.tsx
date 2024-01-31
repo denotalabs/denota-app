2bc07ffe0f8799ddfa0268efcf0aa43aa73289cb
Mohamed Basrai <mohamedalibasrai@gmail.com>
2024-01-31 16:37:57 -0500

    update GQL query

--- frontend/hooks/useNotas.tsx
+++ frontend/hooks/useNotas.tsx
@@ -4,10 +4,6 @@ import { BigNumber } from "ethers";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { NotaCurrency } from "../components/designSystem/CurrencyIcon";
 import { useBlockchainData } from "../context/BlockchainDataProvider";
-import {
-  chainInfoForChainId,
-  chainNumberToChainHex,
-} from "../context/chainInfo";
 
 interface Props {
   notaField: string;
@@ -34,7 +30,6 @@ export type EscrowStatus =
 
 export interface EscrowModuleData {
   status: EscrowStatus;
-  isSelfSigned: boolean;
   module: "escrow";
 }
 
@@ -57,14 +52,9 @@ export interface Nota {
   uri: string;
   payer: string;
   payee: string;
-  dueDate?: Date;
   moduleData: EscrowModuleData | DirectPayModuleData;
   inspector?: string;
   isInspector: boolean;
-  isCrossChain: boolean;
-  sourceChainName?: string;
-  sourceChainHex?: string;
-  destChain?: string;
 }
 
 const convertExponent = (amountExact: number) => {
@@ -112,121 +102,47 @@ export const useNotas = ({ notaField }: Props) => {
     (gqlNota: any) => {
       const createdTx = gqlNota.createdTransaction.id;
 
-      const fundedDate = gqlNota.moduleData.fundedTimestamp
-        ? new Date(Number(gqlNota.moduleData.fundedTimestamp) * 1000)
-        : null;
+      const isPayer =
+        gqlNota.sender.id === blockchainState.account.toLowerCase();
 
-      const fundedTx = gqlNota.moduleData.fundedTransaction
-        ? gqlNota.moduleData.fundedTransaction.id
-        : null;
-
-      const payer = gqlNota.sender.id as string;
-
-      const payee = gqlNota.receiver.id as string;
-
-      const isPayer = payer === blockchainState.account.toLowerCase();
-
-      const isInspector =
-        gqlNota.inspector?.id === blockchainState.account.toLowerCase();
-
-      let dueDate: Date | undefined = undefined;
-
-      if (gqlNota.moduleData.dueDate) {
-        dueDate = new Date(Number(gqlNota.moduleData.dueDate) * 1000);
-      }
+      // TODO: module data
+      const isInspector = false;
 
       let moduleData: EscrowModuleData | DirectPayModuleData;
 
-      if (gqlNota.moduleData.__typename === "DirectPayData") {
-        const status = gqlNota.moduleData.status;
-        let viewerStatus: DirectPayStatus = "awaiting_payment";
-        if (status === "AWAITING_PAYMENT") {
-          if (isPayer) {
-            viewerStatus = "payable";
-          } else {
-            viewerStatus = "awaiting_payment";
-          }
-        } else {
-          viewerStatus = "paid";
-        }
-        moduleData = { module: "direct", status: viewerStatus };
-      } else if (gqlNota.moduleData.__typename === "ReversiblePaymentData") {
-        const status = gqlNota.moduleData.status;
-        let viewerStatus: EscrowStatus = "awaiting_escrow";
-        switch (status) {
-          case "AWAITING_ESCROW":
-            if (isPayer) {
-              viewerStatus = "payable";
-            } else {
-              viewerStatus = "awaiting_escrow";
-            }
-            break;
-          case "AWAITING_RELEASE":
-            if (isInspector) {
-              viewerStatus = "releasable";
-            } else {
-              viewerStatus = "awaiting_release";
-            }
-            break;
-          case "RELEASED":
-            viewerStatus = "released";
-            break;
-          case "VOIDED":
-            viewerStatus = "voided";
-        }
+      const isEscrow = gqlNota.escrowed === 0;
+
+      if (isEscrow) {
+        moduleData = { module: "direct", status: "paid" };
+      } else {
         moduleData = {
           module: "escrow",
-          status: viewerStatus,
-          isSelfSigned: gqlNota.moduleData.isSelfSigned,
+          status: "awaiting_release",
         };
       }
 
-      const sourceChainHex = gqlNota.moduleData.sourceChain
-        ? chainNumberToChainHex(Number(gqlNota.moduleData.sourceChain))
-        : undefined;
-
-      const sourceChainName = gqlNota.moduleData.sourceChain
-        ? chainInfoForChainId(Number(gqlNota.moduleData.sourceChain))
-            .displayName
-        : undefined;
-
-      const destChain = gqlNota.moduleData.sourceChain
-        ? chainInfoForChainId(Number(gqlNota.moduleData.destChain)).displayName
-        : undefined;
+      const amount = isEscrow ? gqlNota.escrowed : gqlNota.escrows[0].instant;
 
       return {
         id: gqlNota.id as string,
-        amount: convertExponent(gqlNota.moduleData.amount as number),
-        amountRaw: BigNumber.from(gqlNota.moduleData.amount),
+        amount: convertExponent(amount as number),
+        amountRaw: BigNumber.from(amount),
         token: currencyForTokenId(gqlNota.erc20.id),
         receiver: gqlNota.receiver.id as string,
         sender: gqlNota.sender.id as string,
         owner: gqlNota.owner.id as string,
         createdTransaction: {
-          date: new Date(Number(gqlNota.timestamp) * 1000),
+          date: new Date(Number(gqlNota.createdTransaction.timestamp) * 1000),
           hash: createdTx,
         },
-        fundedTransaction:
-          fundedDate && fundedTx
-            ? {
-                date: fundedDate,
-                hash: fundedTx,
-              }
-            : null,
+        fundedTransaction: null, // TODO: calculate based on escrow
         uri: gqlNota.uri,
         isPayer,
-        payer,
-        payee,
-        dueDate,
+        payer: gqlNota.sender.id as string,
+        payee: gqlNota.receiver.id as string,
         moduleData,
-        inspector: gqlNota.inspector
-          ? (gqlNota.inspector?.id as string)
-          : undefined,
+        inspector: undefined,
         isInspector,
-        isCrossChain: gqlNota.moduleData.isCrossChain,
-        sourceChainName,
-        destChain,
-        sourceChainHex,
       };
     },
     [blockchainState.account, currencyForTokenId]
@@ -238,9 +154,9 @@ export const useNotas = ({ notaField }: Props) => {
       const tokenFields = `      
       id
       escrowed
-      timestamp
       uri
       createdTransaction {
+        timestamp
         id
       }
       sender {
@@ -255,45 +171,8 @@ export const useNotas = ({ notaField }: Props) => {
       erc20 {
         id
       }
-      inspector {
-        id
-      }
-      moduleData {
-        ... on DirectPayData {
-          __typename
-          status
-          amount
-          fundedTimestamp
-          isCrossChain
-          sourceChain
-          destChain
-          fundedTransaction {
-            id
-          }
-          creditor {
-            id
-          }
-          debtor {
-            id
-          }
-          dueDate
-        }
-        ... on ReversiblePaymentData {
-          __typename
-          status
-          amount
-          fundedTimestamp
-          fundedTransaction {
-            id
-          }
-          isSelfSigned
-          creditor {
-            id
-          }
-          debtor {
-            id
-          }
-        }
+      escrows {
+        instant
       }
       `;
 
@@ -410,10 +289,6 @@ export const useNotas = ({ notaField }: Props) => {
     ) {
       return undefined;
     }
-    const nonSelfSigned = notasInspected.filter(
-      (nota: Nota) =>
-        nota.moduleData.module === "escrow" && !nota.moduleData.isSelfSigned
-    );
     switch (notaField) {
       case "notasSent":
         return notasSentIncludingOptimistic;
@@ -422,7 +297,6 @@ export const useNotas = ({ notaField }: Props) => {
       default:
         return notasReceivedIncludingOptimistic
           .concat(notasSentIncludingOptimistic)
-          .concat(nonSelfSigned)
           .sort((a, b) => {
             return (
               b.createdTransaction.date.getTime() -
